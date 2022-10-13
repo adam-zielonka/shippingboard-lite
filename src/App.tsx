f9f6174200331d8d911446c96a15aea4f0d7ef15
Adam Zielonka <root@adamzielonka.pro>
2022-10-13 21:26:06 +0200

    Add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@blueprintjs/core";
+import { Alignment, Button, Navbar } from "@blueprintjs/core";
 import { observer } from "mobx-react-lite";
 import "./App.css";
 import { store } from "./store/Store";
@@ -6,7 +6,15 @@ import { store } from "./store/Store";
 function App() {
   return (
     <div className="App">
-      <div>
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>ShippingBoard Light</Navbar.Heading>
+          <Navbar.Divider />
+          <Button className="bp4-minimal" icon="dashboard" text="Dashboard" />
+          <Button className="bp4-minimal" icon="th" text="List" />
+        </Navbar.Group>
+      </Navbar>
+      <div className="content">
         <Button text={store.counter} onClick={store.riseCounter} />
       </div>
     </div>
