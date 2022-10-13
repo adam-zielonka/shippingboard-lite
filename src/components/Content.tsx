4c3ec5d1bd1cab13f8675f3079a9d84a0b02e7cf
Adam Zielonka <root@adamzielonka.pro>
2022-10-13 21:54:09 +0200

    Create component for Gate

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@blueprintjs/core";
+import { Button, Card, Elevation } from "@blueprintjs/core";
 import { observer } from "mobx-react-lite";
 import { store } from "../store/Store";
 
+import "./Content.css";
+
 export const Content = observer(() => {
 
-  return <div className="content">
-    <Button text={store.counter} onClick={store.riseCounter} />
+  return <div className="Content">
+    <Gate/><Gate/><Gate/><Gate/><Gate/><Gate/><Gate/><Gate/><Gate/>
   </div>;
+});
+
+
+export const Gate = observer(() => {
+  
+  return <Card interactive={true} elevation={Elevation.TWO}>
+    <header>A</header>
+    <Button text={store.counter} onClick={store.riseCounter} />
+  </Card>;
 });
