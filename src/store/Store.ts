a52f4f7c9d7e397160dba51815c7aa145d735d37
Adam Zielonka <root@adamzielonka.pro>
2022-10-18 05:56:43 +0200

    Clean up store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -3,15 +3,10 @@ import { UI } from "./UI";
 
 export class Store {
   ui = new UI();
-  counter = 0;
 
   constructor() {
     makeAutoObservable(this);
   }
-
-  riseCounter = () => {
-    this.counter++;
-  };
 }
 
 export const store = new Store();
