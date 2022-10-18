e05eb05d37f0df88bbf1ac097fa3ce0fe7ca5dea
Adam Zielonka <root@adamzielonka.pro>
2022-10-18 05:57:11 +0200

    On click open loading

--- src/components/Ramp.tsx
+++ src/components/Ramp.tsx
@@ -1,10 +1,12 @@
 import { Card, Elevation } from "@blueprintjs/core";
 import { observer } from "mobx-react-lite";
+import { store } from "../store/Store";
 import "./Ramp.css";
 
 export const Ramp = observer((ramp: { id: string }) => {
+  const { ui } = store;
   
-  return <Card className="Ramp" interactive={true} elevation={Elevation.TWO}>
+  return <Card className="Ramp" interactive={true} elevation={Elevation.TWO} onClick={ui.openLoadingDialog}>
     <header>{ramp.id}</header>
     <div>RAMP</div>
   </Card>;
