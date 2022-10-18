8c07a37c2881e8225322727a01b2aeba7b9d4642
Adam Zielonka <root@adamzielonka.pro>
2022-10-18 20:31:07 +0200

    Add basic info to loading dialog

--- src/components/LoadingDialog.tsx
+++ src/components/LoadingDialog.tsx
@@ -1,30 +1,39 @@
-import { Button, Classes, Dialog } from "@blueprintjs/core";
+import { Button, Classes, ControlGroup, Dialog, FormGroup, InputGroup, NumericInput, Slider } from "@blueprintjs/core";
 import { observer } from "mobx-react-lite";
+import { useState } from "react";
 import { store } from "../store/Store";
+import "./LoadingDialog.css";
 
 export const LoadingDialog = observer(() => {
   const { ui } = store;
+  const [duration, setDuration] = useState(4);
+  const [pallets, setPallets] = useState(3);
     
   return <Dialog isOpen={ui.isLoadingDialogOpen} title={"Loading"} onClose={ui.closeLoadingDialog}>
     <div className={Classes.DIALOG_BODY}>
-      <p>
-        <strong>
-            Dolorum tempora mollitia enim cupiditate,
-            aut perferendis iure earum reprehenderit cum, eveniet blanditiis ducimus maxime autem quibusdam
-            esse ea unde et distinctio!
-        </strong>
-      </p>
-      <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum tempora mollitia enim cupiditate,
-            aut perferendis iure earum reprehenderit cum, eveniet blanditiis ducimus maxime autem quibusdam
-            esse ea unde et distinctio!
-      </p>
-      <p>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum tempora mollitia enim cupiditate,
-            aut perferendis iure earum reprehenderit cum, eveniet blanditiis ducimus maxime autem quibusdam
-            esse ea unde et distinctio!
-      </p>
-      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+      <div className="datetime">
+        <FormGroup label="Date:" labelFor="date">
+          <InputGroup id="date" value="2022-11-11"/>
+        </FormGroup>
+        <FormGroup label="Time:" labelFor="time">
+          <InputGroup id="time" value="13:14"/>
+        </FormGroup>
+        <FormGroup label="Duration:">
+          <Slider value={duration} min={0} max={8} stepSize={0.5} labelStepSize={2}
+            labelRenderer={value => `${value}h`} onChange={setDuration}/>
+        </FormGroup>
+      </div>
+      <ControlGroup fill>
+        <FormGroup label="Pallets:">
+          <NumericInput value={pallets} onValueChange={setPallets} fill/>
+        </FormGroup>
+        <FormGroup label="Loaded Pallets:">
+          <NumericInput value={pallets} onValueChange={setPallets} fill/>
+        </FormGroup>
+        <FormGroup label="Truck No.:">
+          <InputGroup id="date" value="CP 2022 77" fill/>
+        </FormGroup>
+      </ControlGroup>
     </div>
     <div className={Classes.DIALOG_FOOTER}>
       <div className={Classes.DIALOG_FOOTER_ACTIONS}>
